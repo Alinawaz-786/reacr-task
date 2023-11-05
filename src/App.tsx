6fc2954b87a269dfe113e97de24a7bdabce4bb04
Alinawaz-786 <alinawaz1857@gmail.com>
2023-11-05 02:15:31 -0800

    Basic template

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,25 @@
-import Header from './Header';
-import Nav from './Nav';
-import Footer from './Footer';
-import NewPost from './NewPost';
-import PostPage from './PostPage';
-import About from './About';
-import Home from './Home';
-import Missing from './Missing';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { format } from 'date-fns';
-import api from './api/post';
-import EditPost from './EditPost';
-import { MyProvider } from './context/DataContext'
-// import {useState,useEffect} from 'react';
-
-interface objectProps {
-  id: number,
-  title: string,
-  datetime: string,
-  body: string
-}
-
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  
-
   return (
-    <MyProvider>
-      <div className="App">
-        <Nav  />
-        <Header />
-
-        <>
-          <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/NewPost" element={<NewPost />} />
-            <Route path="/edit-post/:id" element={<EditPost />} />
-            <Route path="/post/:id" element={<PostPage />} />
-            <Route path="/Missing" element={<Missing />} />
-          </Routes>
-        </>
-        <Footer />
-      </div>
-    </MyProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
